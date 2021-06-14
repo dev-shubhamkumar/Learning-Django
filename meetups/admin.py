4d1cf232841d8660b9b77aec68d715531160918a
Shubham Kumar <developer.shubham07@gmail.com>
2021-06-14 12:40:56 +0530

    Adding Image Uploads

--- meetups/admin.py
+++ meetups/admin.py
@@ -4,4 +4,10 @@ from .models import Meetup
 
 # Register your models here. Registering here is important to show a model in Django Admin
 
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_filter = ('title', )
+    prepopulated_fields = {'slug': ('title',)}      ## This is how we auto populate a fiels in django
+
+
+admin.site.register(Meetup, MeetupAdmin)
