affd896540b4f07ae09e829053ebf091721c6e81
Shubham Kumar <developer.shubham07@gmail.com>
2021-06-14 14:46:13 +0530

    Mid Commit

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 
-from .models import Meetup
+from .models import Meetup, Location, Particiant
 
 # Register your models here. Registering here is important to show a model in Django Admin
 
 class MeetupAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    list_filter = ('title', )
+    list_display = ('title', 'date', 'location')
+    list_filter = ('location', 'date')
     prepopulated_fields = {'slug': ('title',)}      ## This is how we auto populate a fiels in django
 
 
 admin.site.register(Meetup, MeetupAdmin)
+admin.site.register(Location)
+admin.site.register(Particiant)
--- meetups/models.py
+++ meetups/models.py
@@ -4,11 +4,33 @@ from django.db import models
 # NOTE: Django automatically create database from models.
 # NOTE: It creates 1 table in database per model
 
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name} ({self.address})'
+
+
+class Particiant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+# NOTE: Building a one-to-many relationship b/w location and meetups
+# i.e. One location can be used for many meetups
+# but not vice versa
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
+    organizer_email = models.EmailField()
+    date = models.DateField()
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)    # If the location gets deleted then Meetup will also get deleted
+    particiant = models.ManyToManyField(Particiant, blank=True, null=True)
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
